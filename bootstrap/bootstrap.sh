ff4cea645d84466fae4aa6e4749f010c2ec021a4
Robert Boll <robertcboll@gmail.com>
2015-03-01 01:57:18 -0500

    misnamed

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -18,8 +18,8 @@ echo "press any key to continue..."
 read
 
 echo "installing plugins and restarting"
-java -jar $CLI -s $URL install-plugin $PLUGINS 2>/dev/null
-java -jar $CLI -s $URL safe-restart 2>/dev/null
+java -jar $CLI -s $URL install-plugin $PLUGINS
+java -jar $CLI -s $URL safe-restart
 
 echo "waiting for jenkins restart"
 sleep 30 # wait 30 seconds for shutdown, before we start polling
@@ -28,16 +28,16 @@ until java -jar $CLI -s $URL wait-node-online "">/dev/null 2>&1; do
 done
 
 echo "creating jenkins folder"
-cat $DIR/jobs/folder.xml | java -jar $CLI -s $URL create-job '_jenkins' 2>/dev/null
+cat $DIR/jobs/folder.xml | java -jar $CLI -s $URL create-job '_jenkins'
 
 echo "creating bootstrap job"
-cat $DIR/jobs/bootstrap.xml | java -jar $CLI -s $URL create-job '_jenkins/bootstrap' 2>/dev/null
+cat $DIR/jobs/bootstrap.xml | java -jar $CLI -s $URL create-job '_jenkins/bootstrap'
 
 echo "triggering bootstrap job"
-java -jar $CLI -s $URL build -f '_jenkins/bootstrap' 2>/dev/null
+java -jar $CLI -s $URL build -f '_jenkins/bootstrap'
 
 echo "waiting for jenkins restart"
-java -jar $CLI -s $URL wait-node-offline "" 2>/dev/null
+java -jar $CLI -s $URL wait-node-offline ""
 
 sleep 30 # wait 30 seconds for shutdown, before we start polling
 until java -jar $CLI -s $URL wait-node-online "">/dev/null 2>&1; do
@@ -46,5 +46,5 @@ done
 
 if [ ! -z $GITHUB_CLIENT_ID ] && [ ! -z $GITHUB_CLIENT_SECRET ]; then
   echo "configuring github auth"
-  java -jar $CLI -s $URL groovy github-auth.groovy $GITHUB_CLIENT_ID $GITHUB_CLIENT_SECRET 2>/dev/null
+  java -jar $CLI -s $URL groovy github-oauth.groovy $GITHUB_CLIENT_ID $GITHUB_CLIENT_SECRET
 fi
