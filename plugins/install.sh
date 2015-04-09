d4d8c1192007fb2ec5cb7665d2e2f7f634531292
Robert Boll <robertcboll@gmail.com>
2015-04-08 23:27:23 -0400

    debug

--- plugins/install.sh
+++ plugins/install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-set -o pipefail
 
 # usage $0 jenkins_url
 # NOTE: jenkins_url won't be populated if it's not set in global jenkins config
@@ -12,7 +11,7 @@ CLI=/tmp/jenkins-cli.jar
 PLUGIN_FILE=$DIR/plugins.list
 PLUGINS=`cat $PLUGIN_FILE | sed ':a;N;$!ba;s/\n/ /g'`
 
-echo "downloading jenkins-cli"
+echo "downloading jenkins-cli from ; $URL/jnlpJars/jenkins-cli.jar"
 curl --silent $URL/jnlpJars/jenkins-cli.jar > $CLI
 
 echo "installing plugins"
