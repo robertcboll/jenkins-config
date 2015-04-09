38164898decee0690e2080639b9fc6870d5059de
Robert Boll <robertcboll@gmail.com>
2015-04-08 23:30:14 -0400

    accomodate changes

--- plugins/install.sh
+++ plugins/install.sh
@@ -3,7 +3,7 @@
 # usage $0 jenkins_url
 # NOTE: jenkins_url won't be populated if it's not set in global jenkins config
 
-URL=${1:-http://localhost:8080}
+URL=${1:-http://localhost:8080/}
 DIR=$(dirname $0)
 
 CLI=/tmp/jenkins-cli.jar
@@ -11,8 +11,8 @@ CLI=/tmp/jenkins-cli.jar
 PLUGIN_FILE=$DIR/plugins.list
 PLUGINS=`cat $PLUGIN_FILE | sed ':a;N;$!ba;s/\n/ /g'`
 
-echo "downloading jenkins-cli from ; $URL/jnlpJars/jenkins-cli.jar"
-curl --silent $URL/jnlpJars/jenkins-cli.jar > $CLI
+echo "downloading jenkins-cli from ; ${URL}jnlpJars/jenkins-cli.jar"
+curl --silent ${URL}jnlpJars/jenkins-cli.jar > $CLI
 
 echo "installing plugins"
 java -jar $CLI -s $URL install-plugin $PLUGINS -restart 2>/dev/null
